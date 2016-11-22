a2ffa3073d258a8ffeca946bc1e0f9e63e4bb803
Cordelia Foo <cordeliafoo@cordyfoo.local>
2016-11-22 17:00:21 +0800

    added if loops

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,14 @@ var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
+
+//check for equality between two cards
+
+if (cardOne === cardTwo || cardThree === cardFour){
+
+	alert("You found a match!");
+	
+} else if (cardOne === cardThree || cardTwo === cardFour) {
+
+	alert("Sorry, try again.");
+}
