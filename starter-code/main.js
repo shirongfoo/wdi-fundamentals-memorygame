32ecd2e1f01dee3f903576facb00fe8ebf896ddc
Cordelia Foo <cordeliafoo@cordyfoo.local>
2016-11-24 23:09:29 +0800

    completed code for memorygame

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,34 +1,55 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
-//check for equality between two cards
-
-/*alert to inform user if they picked matching cards
-if (cardOne === cardTwo || cardThree === cardFour){
-
-	alert("You found a match!");
-	
-} else if (cardOne === cardThree || cardTwo === cardFour) {
-
-	alert("Sorry, try again.");
+var createBoard = function (){
+		
+		for (var i = 0; i < cards.length; i+=1) {
+		var card = document.createElement('div')
+		card.className = "card";
+		gameBoard.appendChild(card);
+		card.setAttribute('data-card', cards[i]); //'data-card' is the name of the attribute, and cards[i] provides the value to that attribute
+		card.addEventListener('click', isTwoCards);
+		}
 }
-*/
+
 
 
 var gameBoard = document.getElementById("game-board");
 
-var createCards = function (numOfCards){
-		
-		for (var i = 0; i < numOfCards; i+=1) {
-		var card = document.createElement('div')
-		card.className = "card";
-		gameBoard.appendChild(card);
-		}
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = '<img src = king.png>';
+	} else {
+		this.innerHTML = '<img src = queen.png>';
+	}
+
+	if (cardsInPlay.length ===2){
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}	
+
 }
 
-createCards(4);
+var isMatch = function(cards){
+	if (cards[0] === cards[1]) {
+		alert ("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+
+createBoard(cards);
+
+
+
+
+
+
+
 
